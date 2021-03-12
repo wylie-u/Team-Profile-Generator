e11577eae25ce2b82f24bc4a5e89149ba5b79706
wylie-u <wylieurig@gmail.com>
2021-03-12 16:09:53 -0700

    reworking html file

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const Engineer = require("./library/engineer");
 const Manager = require("./library/manager");
 const Intern = require("./library/intern");
 
+// const style = require("./dist/style.css");
+
 // this will be for length of array
 let teamNumber = [];
+
 
 //Inquirer prompt for manager: name, employee ID, email address, and office number
 
 const managerFile = () =>
+// works when there is a console log
   inquirer
     .prompt([
       // manager name
@@ -38,7 +42,8 @@ const managerFile = () =>
       const email = answers.email;
       const officeNumber = answers.officeNumber;
       // calling manager class
-      const teamMember = new Manager(name, id, email, officeNumber);
+      const manager = new Manager(name, id, email, officeNumber);
+      teamNumber.push(manager)
       addMember();
     });
 
@@ -46,7 +51,7 @@ const managerFile = () =>
 // then(
 //1a. create manager object from Manager class with the data just entered: const manager = new Manager (the data based on the class properties);
 
-//1b. call the function which prompt for 3 choices: engineer, intern or done)
+
 
 const addMember = () =>
   inquirer
@@ -106,7 +111,8 @@ const engineerFile = () =>
       const email = answers.email;
       const github = answers.github;
       // calling engineer class
-      const teamMember = new Engineer(name, id, email, github);
+      const engineer = new Engineer(name, id, email, github);
+      teamNumber.push(engineer)
       addMember();
     });
 
@@ -136,31 +142,57 @@ const internFile = () =>
       const email = answers.email;
       const school = answers.school;
       // calling intern class
-      const teamMember = new Intern(name, id, email, school);
+      const intern = new Intern(name, id, email, school);
+      teamNumber.push(intern)
       addMember();
     });
 
-//Choice is Engineer:
-//call the engineer prompts by calling the function for engineer (structure is similar to how we created the manager's function: prompts, then create object from engineer class, then call the function which prompt for 3 choices: engineer, intern or done)
+    //html 
 
-//Same for Intern
+    const generateHTML = (answers) =>
 
-//Done: call html function, values will go into cards
+    
+`<body>
+<div class="jumbotron jumbotron-fluid">
+      <h1>My Team</h1>
+  </div>
+  <div class="team-container">
+    <div class="row team">
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+      </div>
+      
+          
+          
+      
+</body>
+</html>`
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
+    // write file
+
+managerFile ();
